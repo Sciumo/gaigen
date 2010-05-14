e1b12554d3f3238d45d07c5f5058e1dc16fa5c9a
fontijne <fontijne@ef7de014-d8c2-4133-94b5-072587736ee7>
2010-05-14 12:07:41 +0000

    Finished ToString method for CSharp

--- g25/spec_conf_csharp/Program.cs
+++ g25/spec_conf_csharp/Program.cs
@@ -14,7 +14,7 @@ namespace spec_conf_csharp
 		    double[] coordinates = new double[] {-20.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 0.000000001};
 		    mv X = new mv(gu, coordinates);
     		
-		    String str = c3ga.String(X, "%e");
+		    String str = c3ga.String(X, "E");
     		
             Console.WriteLine(str);
 		    Console.WriteLine(X);
