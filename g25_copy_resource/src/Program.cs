6387006c76279735b40cb63d1e31c4be6c6fb4dc
fontijne <fontijne@ef7de014-d8c2-4133-94b5-072587736ee7>
2010-07-04 18:47:43 +0000

    Working on test suite for C#, need program to copy dll from installation directory to arbitrary location.

---
+++ g25_copy_resource/src/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace g25_copy_resource
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
