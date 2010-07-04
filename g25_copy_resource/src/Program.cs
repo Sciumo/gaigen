6614a230708fc9d1ee0c0fb1596bef69d7f55aa5
fontijne <fontijne@ef7de014-d8c2-4133-94b5-072587736ee7>
2010-07-04 19:07:46 +0000

    Added small utility program to copy installed files to any location (g25_copy_resource). To be used for C# test suite and Java test Suite to copy ANTLR runtime.

--- g25_copy_resource/src/Program.cs
+++ g25_copy_resource/src/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace g25_copy_resource
 {
@@ -8,6 +9,31 @@ namespace g25_copy_resource
     {
         static void Main(string[] args)
         {
+            // check command line arguments
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: g25_copy_resource source destination");
+                Environment.Exit(0);
+            }
+
+            // get path to where g25 is installed
+            string exePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string g25InstallDir = Path.GetDirectoryName(exePath);
+
+            // get path to source file
+            string source = Path.Combine(g25InstallDir, args[0]);
+
+            // get path to destination file
+            string dest = args[1];
+            if (System.IO.Directory.Exists(dest))
+                dest = Path.Combine(dest, args[0]);
+
+
+            // make sure dest goes away (copy will not overwrite)
+            File.Delete(dest);
+
+            // copy file
+            File.Copy(source, dest);
         }
     }
 }
